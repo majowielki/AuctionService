c031a1572bc5ebbccab6f6cb648d60092b3a2100
majowielki <mmajewski111@gmail.com>
2024-11-10 16:33:09 +0100

    Move services into one sln

---
+++ src/SearchService/Consumers/AuctionCreatedConsumer.cs
@@ -0,0 +1,28 @@
+﻿using MassTransit;
+using Contracts;
+using AutoMapper;
+using SearchService.Models;
+using MongoDB.Entities;
+
+namespace SearchService.Consumers
+{
+    public class AuctionCreatedConsumer : IConsumer<AuctionCreated>
+    {
+        private readonly IMapper _mapper;
+
+        public AuctionCreatedConsumer(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+        public async Task Consume(ConsumeContext<AuctionCreated> context)
+        {
+            Console.WriteLine("--> Consuming auction created: " + context.Message.Id);
+
+            var item = _mapper.Map<Item>(context.Message);
+
+
+
+            await item.SaveAsync();
+        }
+    }
+}
---
+++ src/SearchService/Program.cs
@@ -0,0 +1,56 @@
+using Polly;
+using Polly.Extensions.Http;
+using SearchService.Data;
+using SearchService.Services;
+using MassTransit;
+using SearchService.Consumers;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
+builder.Services.AddMassTransit(x =>
+{
+    x.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
+
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("search", false));
+
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.ReceiveEndpoint("search-auction-created", e =>
+        {
+            e.UseMessageRetry(r => r.Interval(5, 5));
+            e.ConfigureConsumer<AuctionCreatedConsumer>(context);
+        });
+
+        cfg.ConfigureEndpoints(context);
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Lifetime.ApplicationStarted.Register(async () =>
+{
+    try
+    {
+        await DbInitializer.InitDb(app);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+});
+
+app.Run();
+
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+    => HttpPolicyExtensions.HandleTransientHttpError().OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.NotFound)
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
